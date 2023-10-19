dc24291b10dd04b0f5768a878022236b87291ea3
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-10-19 17:24:43 +0600

    abstruct class 2

--- abstruct.php
+++ abstruct.php
@@ -1,7 +1,7 @@
 <?php
 
 // we can not make it object becaseu it's abstruct class. we can only access if exits any static value.
-// we
+// we can only access this abstruct class if we extends this class another normal class.
 abstract class D {
     public static $name = 'Hello';
     public $hi = 'Hi';
