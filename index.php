7a697dc8ec7537147be7cd7d4c55dd0bcb0d603d
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-10-19 13:41:55 +0600

    extends part 01

--- index.php
+++ index.php
@@ -1,31 +1,41 @@
 <?php
 
 
-class NewClass {
+class ClassOne {
     public static $name = 'nahian';
-    public $skill = 'Backend Developer';
+    public $skill1 = 'Backend Developer';
 
     public static function info(){
-        echo 'hello '.self::$name;
+        echo 'this is '.self::$name;
     }
 
     
     public function fun2(){
-        echo 'sklls are '.$this->skill;
+        echo 'sklls are '.$this->skill1;
     }
 
-    public function __construct()
-    {
-       echo '__construct is called before everything <hr> '; 
-    }
+    // public function __construct()
+    // {
+    //    echo '__construct is called before everything <hr> '; 
+    // }
 
-    public function __destruct()
-    {
-        echo '<hr>  __destruct is called after everything'; 
+    // public function __destruct()
+    // {
+    //     echo '<hr>  __destruct is called after everything'; 
+    // }
+}
+
+class ClassTwo extends ClassOne {
+    public static $name = 'rahi';
+    public $skill = 'Frontend Developer';
+
+    public static function info(){
+        echo 'this is '.self::$name;
     }
 }
 
 // echo NewClass::info(); 
-$obj = new NewClass;
-echo $obj->fun2();
+$obj = new ClassTwo;
+echo $obj->info();
+echo $obj->skill1;
 
