916d3997e36f0dd579adfd70b4755383f89873e4
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-10-21 20:34:23 +0600

    namespace feature

---
+++ namespace.php
@@ -0,0 +1,8 @@
+<?php
+
+/** 
+ *  namespace is just a location of the class.
+ *  When we want to use this specific class anywhere else, we need to use this namespace of this class for connection.
+ *  namespace is like "App\Controllers\User"
+ */
+
