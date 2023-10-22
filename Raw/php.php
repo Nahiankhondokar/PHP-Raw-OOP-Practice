675db932493fe8d1e4bd6561161da0fb7be546cc
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-10-22 08:24:13 +0600

    raw php intro

---
+++ Raw/php.php
@@ -0,0 +1,6 @@
+<?php 
+
+$previous_name = 'personal home page. it is discovered at 1994';
+$php = 'hyper text pre processor';
+$server = 'php is a server site scripting language, php work in server.';
+$ASP_lun = 'ASP is a server site language which is build by Microsoft';
